bc24a3553244490012a5212ba5dc63dbe273d0a6
Eduarda <eduarda.neumann@wr.sc.usp.br>
2023-07-13 17:13:25 -0300

    Adapted to use csv for question and answers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 
@@ -10,9 +11,14 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
 
+df = pd.read_csv('in.csv')
+dic = dict(zip(df.questions, df.answers))
+
 @bot.event
 async def on_message(message):
-	if message.content == "hello":
-		await message.channel.send("hey")
+	answer = dic.get(message.content)
+	if answer is not None:
+	 	await message.channel.send(answer)
+
 
 bot.run(DISCORD_TOKEN)
